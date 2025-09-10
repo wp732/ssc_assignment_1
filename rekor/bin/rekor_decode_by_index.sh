9e25ee816c99b86635ff33484cf4e10bc95b0eb4
Warren Pfeffer <wp732@nyu.edu>
2025-09-09 20:39:04 -0700

    added body and message dumps

--- rekor/bin/rekor_decode_by_index.sh
+++ rekor/bin/rekor_decode_by_index.sh
@@ -2,6 +2,28 @@
 
 thisdir=`(cd \`dirname $0\` > /dev/null 2>&1; pwd)`
 
+pass_args=""
+use_verify=0
+use_print_msg=0
+use_print_body=0
+while [ $# -ne 0 ]; do
+	case $1 in
+		-v )
+			use_verify=1
+			;;
+		-m )
+			use_print_msg=1
+			;;
+		-b )
+			use_print_body=1
+			;;
+		* )
+			pass_args="${pass_args} $1"
+			;;
+	esac
+	shift
+done
+
 tmp_dir=/tmp/`uuid`
 mkdir -p $tmp_dir
 
@@ -9,7 +31,7 @@ rekor_pub=${tmp_dir}/rekor.pub
 curl -s https://rekor.sigstore.dev/api/v1/log/publicKey > $rekor_pub
 
 rekor_entry=${tmp_dir}/rekor_entry.json
-${thisdir}/rekor_by_index.sh $* > $rekor_entry
+${thisdir}/rekor_by_index.sh $pass_args > $rekor_entry
 
 rekor_entry_set=${tmp_dir}/rekor_entry_set.txt
 jq -r '.[].verification.signedEntryTimestamp' $rekor_entry > $rekor_entry_set
@@ -20,6 +42,16 @@ base64 -d $rekor_entry_set > $rekor_entry_set_decoded
 rekor_entry_message=${tmp_dir}/rekor_entry_message.txt
 jq -cj '.[] | del(.attestation, .verification)' $rekor_entry > $rekor_entry_message
 
-openssl dgst -sha256 -verify $rekor_pub -signature $rekor_entry_set_decoded $rekor_entry_message
+if [ $use_verify -eq 1 ]; then
+	openssl dgst -sha256 -verify $rekor_pub -signature $rekor_entry_set_decoded $rekor_entry_message
+fi
+
+if [ $use_print_msg -eq 1 ]; then
+	cat $rekor_entry_message
+fi
+
+if [ $use_print_body -eq 1 ]; then
+	jq -r '.body' $rekor_entry_message | base64 -d
+fi
 
 rm -r -f $tmp_dir
