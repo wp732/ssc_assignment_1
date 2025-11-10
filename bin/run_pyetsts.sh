3c38ffff1a72be5a99bd625e5ffb1aa69bee6f46
Warren Pfeffer <wp732@nyu.edu>
2025-11-10 11:24:43 -0700

    added show output option to run_pyetsts.sh

--- bin/run_pyetsts.sh
+++ bin/run_pyetsts.sh
@@ -2,6 +2,16 @@
 
 thisdir=`(cd \`dirname $0\` > /dev/null 2>&1; pwd)`
 
+show_output=""
+while [ $# -ne 0 ]; do
+    case $1 in
+        -s )
+            show_output="-s"
+            ;;
+    esac
+    shift
+done
+
 cd ${thisdir}/..
 
-poetry run pytest tests/
+poetry run pytest $show_output tests/
