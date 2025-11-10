782cbe31197881a157e213cdce3a0275a319227e
Warren Pfeffer <wp732@nyu.edu>
2025-11-09 18:48:25 -0700

    added more get dir funcs to run_test_wrappers.py

--- tests/test_checkpoint.py
+++ tests/test_checkpoint.py
@@ -1,8 +1,8 @@
-from run_test_wrappers import get_test_dir, validate_json_output
+from run_test_wrappers import get_test_dir, get_src_dir, validate_json_output
 
 def test_checkpoint():
     test_dir = get_test_dir()
-    src_dir = f"{test_dir}/../rekor/src"
+    src_dir = get_src_dir('rekor')
     validate_json_output(
         f"{test_dir}/checkpoint_schema.json",
         f"{src_dir}/main.py",
