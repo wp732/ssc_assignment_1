793a003d56464b1f70b4602b882cb5d6154217d8
Warren Pfeffer <wp732@nyu.edu>
2025-11-17 07:04:38 -0700

    moved tests dir into rekor dir

--- bin/run_pyetsts.sh
+++ bin/run_pyetsts.sh
@@ -2,22 +2,49 @@
 
 thisdir=`(cd \`dirname $0\` > /dev/null 2>&1; pwd)`
 
+usage() {
+	echo ""
+	echo "usage: $0 -service <service name> [-verbose] [-file <py file path>]"
+	echo ""
+	echo "       -service <service name> # Specify service directory name within this project"
+	echo "                               # For example rekor to act on rekor/src"
+	echo "       -verbose                # Force stdout/stderr to tty"
+	echo "       -file <py file path>    # Path to .py file to act on (default all files)"
+	echo ""
+}
+
 show_output=""
 py_file=""
+service=""
 while [ $# -ne 0 ]; do
     case $1 in
-        -s )
+        -verbose )
             show_output="-s"
             ;;
-        -f )
+        -file )
 			shift
             py_file=$1
             ;;
+        -service )
+            shift
+            service=$1
+            ;;
+		-h )
+			usage
+			;;
+		* )
+			usage
+			;;
     esac
     shift
 done
 
+if [ -z "${service}" ]; then
+    echo "ERROR: must supply -service <service name>" >&2
+    exit 255
+fi
+
 cd ${thisdir}/..
 
 set -x
-poetry run pytest $show_output tests/${py_file}
+poetry run pytest $show_output ${service}/tests/${py_file}
