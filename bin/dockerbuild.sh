cb0cb2896d10d7e4adfa723d4c58f77f473c281d
Warren Pfeffer <wp732@nyu.edu>
2025-11-28 17:34:57 -0700

    container builds with poetry

--- bin/dockerbuild.sh
+++ bin/dockerbuild.sh
@@ -4,13 +4,13 @@ thisdir=`(cd \`dirname $0\` > /dev/null 2>&1; pwd)`
 
 source ${thisdir}/dockersetsrc.sh
 
-cd $proj_dir
+cd $src_dir
 
+	#-f ${src_dir}/Dockerfile \
 export DOCKER_BUILDKIT=1
 
 docker build \
 	--build-arg UID=$(id -u) \
 	--build-arg GID=$(id -g) \
 	-t ${CNAME}:${CTAG} \
-	-f ${src_dir}/Dockerfile \
-	.
+	. 2>&1 | cat -
