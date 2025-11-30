3f3a898d144d05aac5d43783fbde69b5f37ef391
Warren Pfeffer <wp732@nyu.edu>
2025-11-29 23:36:59 -0700

    only add missing import packages for package sources

--- bin/add_missing_imports.sh
+++ bin/add_missing_imports.sh
@@ -7,7 +7,11 @@ source ${thisdir}/in_docker.sh
 cd $proj_dir
 
 while read pkg_path; do
+	pushd $pkg_path > /dev/null 2>&1
+	echo "INFO: Adding any missing import packages for ${pkg_path}"
 	while read missing_import; do
+		echo "INFO: Adding missing package ${missing_import}"
 		poetry add $missing_import
 	done< <(${thisdir}/show_missing_imports.sh $pkg_path)
+	popd > /dev/null 2>&1
 done< <(find packages/ -mindepth 1 -maxdepth 1 -type d)
