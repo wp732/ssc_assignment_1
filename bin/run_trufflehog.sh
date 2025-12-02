b521b8abd8e593a79bd77b5a2364f6f067a139f9
Warren Pfeffer <wp732@nyu.edu>
2025-12-01 17:19:59 -0700

    added trufflehog to CI

---
+++ bin/run_trufflehog.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Wrapper script to run Trufflehog secrets scanner
+# using the local installed trufflehog binary.
+
+thisdir=`(cd \`dirname $0\` > /dev/null 2>&1; pwd)`
+
+source ${thisdir}/in_docker.sh
+ 
+cd ${thisdir}/..
+
+export TERM=dumb
+export PYTHONUNBUFFERED=1
+trufflehog git \
+	--no-update \
+	--log-level=5 \
+	--no-verification \
+	--no-color \
+	--branch HEAD \
+	--fail \
+	file:///repo
